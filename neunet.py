55765544a149dec6439238698644a3e49ab036cf
Patrick <pcdiem@verizon.net>
2023-05-22 18:29:28 -0400

    Update neunet.py

--- neunet.py
+++ neunet.py
@@ -1,7 +1,8 @@
 import numpy as np
+from scipy.special import expit
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return expit(x)
 
 def sigmoid_derivative(x):
         return sigmoid(x) * (1 - sigmoid(x))
