11e63177ad1cb0a9a4be9de2b11da5238b00ca4d
Patrick <pcdiem@verizon.net>
2023-05-22 17:48:14 -0400

    Update neunet.py

--- neunet.py
+++ neunet.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class NeuralNetwork:
     def __init__(self, x, y):
         self.input      = x
@@ -5,6 +7,7 @@ class NeuralNetwork:
         self.weights2   = np.random.rand(4,1)                 
         self.y          = y
         self.output     = np.zeros(y.shape)
+
     def feedforward(self):
         self.layer1 = sigmoid(np.dot(self.input, self.weights1))
         self.output = sigmoid(np.dot(self.layer1, self.weights2))
